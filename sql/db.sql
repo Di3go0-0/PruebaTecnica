fcce2011f732ed4d0d69d873e26e9632215bc577
Di3go0-0 <d.rincon@utp.edu.co>
2024-06-14 11:25:05 -0500

    Migrations y seeders de la base de datos

--- sql/db.sql
+++ sql/db.sql
@@ -30,3 +30,16 @@ CREATE TABLE Tasks (
     projectId INT,
     FOREIGN KEY (projectId) REFERENCES Projects(id)
 );
+
+
+INSERT INTO Users (name, email, password, rol) VALUES ('Admin', 'admin@example.com', '123456', 'admin');
+INSERT INTO Users (name, email, password, rol) VALUES ('User1', 'user1@example.com', '123456', 'user');
+INSERT INTO Users (name, email, password, rol) VALUES ('User2', 'user2@example.com', '123456', 'user');
+
+INSERT INTO Projects (name, description, startDate, finalDate, state, userId) VALUES ('Project1', 'Description', '2024-06-21', '2024-07-04', 'In progress', 1);
+INSERT INTO Projects (name, description, startDate, finalDate, state, userId) VALUES ('Project2', 'Description', '2024-06-21', '2024-07-04', 'No started', 2);
+INSERT INTO Projects (name, description, startDate, finalDate, state, userId) VALUES ('Project3', 'Description', '2024-06-21', '2024-07-04', 'Completed', 2);
+
+INSERT INTO Tasks (name, description, creationDate, updateDate, state, projectId) VALUES ('Task1', 'Description', '2024-06-22', '2024-07-06', 'In progress', 1);
+INSERT INTO Tasks (name, description, creationDate, updateDate, state, projectId) VALUES ('Task2', 'Description', '2024-06-22', '2024-07-06', 'Pending', 2);
+INSERT INTO Tasks (name, description, creationDate, updateDate, state, projectId) VALUES ('Task3', 'Description', '2024-06-22', '2024-07-06', 'Completed', 2);
